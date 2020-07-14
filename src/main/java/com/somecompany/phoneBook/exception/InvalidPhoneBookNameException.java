191dc3b250f3e5e69fd3f917c7a07b94ff12cf88
Patrick Cheung <patrick_cheung23@hotmail.com>
2020-07-14 22:04:42 +1000

    Update error message for invalid phone book name

--- src/main/java/com/somecompany/phoneBook/exception/InvalidPhoneBookNameException.java
+++ src/main/java/com/somecompany/phoneBook/exception/InvalidPhoneBookNameException.java
@@ -12,6 +12,6 @@ public class InvalidPhoneBookNameException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public InvalidPhoneBookNameException() {
-		super("Invalid phoneBook name!");
+		super("Invalid phoneBook name! Provide \"A\" for phoneBookA, \"B\" for phoneBookB.");
 	}
 }
